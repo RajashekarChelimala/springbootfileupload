8ad2158dee2cf46de06aca81b6dcac98ccf55d7a
cggtsd <cggtsds59@gmail.com>
2023-09-02 14:16:48 +0530

    commit 1

---
+++ src/main/java/cgg/springbootfileupload/SpringbootfileuploadApplication.java
@@ -0,0 +1,12 @@
+package cgg.springbootfileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootfileuploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootfileuploadApplication.class, args);
+    }
+}
---
+++ src/test/java/cgg/springbootfileupload/springbootfileupload/SpringbootfileuploadApplicationTests.java
@@ -0,0 +1,13 @@
+package cgg.springbootfileupload.springbootfileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootfileuploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
